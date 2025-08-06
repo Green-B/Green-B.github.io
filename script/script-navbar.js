7f751cc7f8c1fb8a35e36e1fe2b6bea8a4baa7d3
Brett Green <green.brett.r@gmail.com>
2025-08-05 19:00:48 -0700

    Initial push to go live

---
+++ script/script-navbar.js
@@ -0,0 +1,17 @@
+document.getElementById("navbar").innerHTML = `
+<a class="navlink" href="Green-B.github.io/index.html"><div class="navbarbutton">
+	Home
+</div></a>
+<a class="navlink" href="https://green-b.github.io/about-me.html"><div class="navbarbutton">
+	About me
+</div></a>
+<a class="navlink" href="https://Green-B.github.io/career.html"><div class="navbarbutton">
+	Career
+</div></a>
+<a class="navlink" href="https://Green-B.github.io/writing.html"><div class="navbarbutton">
+	Writing
+</div></a>
+<a class="navlink" href="https://Green-B.github.io/fun.html"><div class="navbarbutton">
+	Fun
+</div></a>
+`
